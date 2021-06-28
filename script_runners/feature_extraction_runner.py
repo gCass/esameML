c7bf0d6571cb6c4f63f0b4d20f00737716498493
gCass <guglielmo.cassini01@universitadipavia.it>
2021-06-29 01:16:36 +0200

    added feature extraction runner

--- script_runners/feature_extraction_runner.py
+++ script_runners/feature_extraction_runner.py
@@ -1,4 +1,4 @@
 #Feature extraction runner
 import os
 for chosen_number in range(0, 4):
-	os.system('python3 ../feature_extraction.py %d' % chosen_number)
+	os.system('python3 feature_extraction.py %d' % chosen_number)
