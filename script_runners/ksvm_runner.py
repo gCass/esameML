b7d58d6d4d8997924659aa1669b799bf6b988f6d
gCass <guglielmo.cassini01@universitadipavia.it>
2021-06-29 12:39:44 +0200

    Add helping print

--- script_runners/ksvm_runner.py
+++ script_runners/ksvm_runner.py
@@ -13,6 +13,7 @@ lrs = [0.001, 0.002, 0.003, 0.01, 0.02, 0.03, 0.1 , 0.2, 0.3]
 for chosen_number in range(0, 4):
 	## EXTRACT RANDOMLY 3 TIMES FOR EACH CHOSEN NUMBER A SET OF HYPERPARAMETERS
 	for i in range(0,3):
+		print("Doing ",chosen_number, i)
 		lambda_ = random.choice(lambdas)
 		gamma = random.choice(gammas)
 		lr = random.choice(lrs)
