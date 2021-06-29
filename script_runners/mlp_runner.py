b1ed440af9e9cd9a032884815bf8d15a67061dbe
gCass <guglielmo.cassini01@universitadipavia.it>
2021-06-29 15:12:08 +0200

    Added also to iterate over deep feature

--- script_runners/mlp_runner.py
+++ script_runners/mlp_runner.py
@@ -7,7 +7,7 @@ batch_sizes = [10,20,30,40,50]
 lrs = [0.001, 0.002, 0.003, 0.01, 0.02, 0.03, 0.1 , 0.2, 0.3]
 
 # LINEAR
-for chosen_number in range(0, 4):
+for chosen_number in range(0, 5):
 	for epochs in epochs_array:
 		for batch_size in batch_sizes:
 			for lr in lrs:
